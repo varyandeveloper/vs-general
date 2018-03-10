bfa0322e136b4f7e3dcf313bf15561c83f519894
Varazdat Stepanyan <developervaryan@gmail.com>
2018-03-10 13:14:06 -0800

    add general class not found exception

---
+++ src/Exceptions/ClassNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace VS\General\Exceptions;
+
+/**
+ * Class ClassNotFoundException
+ * @package VS\General\Exceptions
+ * @author Varazdat Stepanyan
+ */
+class ClassNotFoundException extends \Exception
+{
+
+}
